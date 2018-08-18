e0d3fb7064a05939331c4ef5fdd9a63e60609036
Richard Darst <rkd@zgib.net>
2018-08-18 18:05:41 +0300

    Add disable-formgrader.sh

---
+++ disable_formgrader.sh
@@ -0,0 +1,4 @@
+jupyter nbextension disable --sys-prefix formgrader/main --section=tree
+jupyter serverextension disable --sys-prefix nbgrader.server_extensions.formgrader
+jupyter nbextension disable --sys-prefix create_assignment/main
+
