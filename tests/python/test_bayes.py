99cdafe93da3ba597c99c5e292328209a9f709b1
Richard Darst <rkd@zgib.net>
2018-10-10 10:54:26 +0300

    test_bayes.py: Update for pystan depreciation

--- tests/python/test_bayes.py
+++ tests/python/test_bayes.py
@@ -1,8 +1,18 @@
 
-def test_bayes():
+## Stops working in stan 3.0
+#def test_bayes_old():
+#    import numpy as np
+#    import pystan
+#    fit = pystan.stan(model_code="parameters {real theta;} model {theta ~ normal(0,1);}")
+#    samples = fit.extract(permuted=True)
+#    assert np.mean(samples['theta']) < 1
+
+def test_bayes_new():
     import numpy as np
     import pystan
-    fit = pystan.stan(model_code="parameters {real theta;} model {theta ~ normal(0,1);}")
+    sm = pystan.StanModel(model_code="parameters {real theta;} model {theta ~ normal(0,1);}")
+    #fit = pystan.stan(model_code="parameters {real theta;} model {theta ~ normal(0,1);}")
+    fit = sm.sampling()
     samples = fit.extract(permuted=True)
     assert np.mean(samples['theta']) < 1
 
