33632dac285bea2346ca6031996430c68eb02f8a
Richard Darst <rkd@zgib.net>
2018-12-31 15:01:37 +0200

    start.sh: Add both user and root hook directories
    - Upstream PR: https://github.com/jupyter/docker-stacks/pull/787
    - This works and is useful for us, for now...

--- start.sh
+++ start.sh
@@ -41,6 +41,7 @@ run-hooks /usr/local/bin/start-notebook.d
 
 # Handle special flags if we're root
 if [ $(id -u) == 0 ] ; then
+    run-hooks /usr/local/bin/start-notebook-root.d
 
     # Only attempt to change the jovyan username if it exists
     if id jovyan &> /dev/null ; then
@@ -103,9 +104,11 @@ if [ $(id -u) == 0 ] ; then
     # the environment preserved
     eval "$NB_PRE_START_HOOK"
     run-hooks /usr/local/bin/before-notebook.d
+    run-hooks /usr/local/bin/before-notebook-root.d
     echo "Executing the command: ${cmd[@]}"
-    exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=/home/$NB_USER/.cache PYTHONPATH=$PYTHONPATH "${cmd[@]}"
+    exec sudo -E -H -u $NB_USER PATH=$PATH XDG_CACHE_HOME=/home/$NB_USER/.cache PYTHONPATH=$PYTHONPATH bash "$BASH_SOURCE" "${cmd[@]}"
 else
+    run-hooks /usr/local/bin/start-notebook-user.d
     if [[ "$NB_UID" == "$(id -u jovyan)" && "$NB_GID" == "$(id -g jovyan)" ]]; then
         # User is not attempting to override user/group via environment
         # variables, but they could still have overridden the uid/gid that
@@ -148,6 +151,7 @@ else
     # Execute the command
     eval "$NB_PRE_START_HOOK"
     run-hooks /usr/local/bin/before-notebook.d
+    run-hooks /usr/local/bin/before-notebook-user.d
     echo "Executing the command: ${cmd[@]}"
     exec "${cmd[@]}"
 fi
