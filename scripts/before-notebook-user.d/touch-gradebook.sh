65caa8b3bc142b5ba323597be4b5f8e3c8fc5e40
Richard Darst <rkd@zgib.net>
2019-01-02 01:56:47 +0200

    scripts/.../touch-gradebook.sh: comment explaining why

--- scripts/before-notebook-user.d/touch-gradebook.sh
+++ scripts/before-notebook-user.d/touch-gradebook.sh
@@ -1,5 +1,9 @@
 # Touch /course/gradebook.db if it does not exist.
 
+# This is needed because sqlite3 will always make the gradebook
+# without g+w permissions, which breaks our sharing of courses.  Our
+# solution is to touch and chmod the gradebook.db before we start.
+
 if [ -d /course ] ; then
     if [ ! -e /course/gradebook.db ] ; then
 	touch /course/gradebook.db
