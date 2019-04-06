a930cd0d44268f9f32f0ef73bfef36333c9936eb
Richard Darst <rkd@zgib.net>
2019-04-06 19:21:53 +0300

    nbgrader_config appending: add line ending safety

--- scripts/before-notebook-user.d/nbgrader_config.sh
+++ scripts/before-notebook-user.d/nbgrader_config.sh
@@ -1,3 +1,6 @@
 if [ -r /course/etc.jupyter.nbgrader_config.py.append -a -w /etc/jupyter/nbgrader_config.py ] ; then
+    echo >> /etc/jupyter/nbgrader_config.py
+    echo >> "# Appended from /course/etc.jupyter.nbgrader_config.py.append"
     cat /course/etc.jupyter.nbgrader_config.py.append >> /etc/jupyter/nbgrader_config.py
+    echo >> "# end appended data"
 fi
