358c11ff83559c7d5c96aa8aa63da67011f49a22
Richard Darst <rkd@zgib.net>
2019-08-05 23:19:32 +0300

    hooks/before-user/nbgrader_config.sh: Fix syntax error

--- hooks/before-notebook-user.d/nbgrader_config.sh
+++ hooks/before-notebook-user.d/nbgrader_config.sh
@@ -1,6 +1,6 @@
 if [ -r /course/etc.jupyter.nbgrader_config.py.append -a -w /etc/jupyter/nbgrader_config.py ] ; then
     echo >> /etc/jupyter/nbgrader_config.py
-    echo >> "# Appended from /course/etc.jupyter.nbgrader_config.py.append"
+    echo "# Appended from /course/etc.jupyter.nbgrader_config.py.append" >> /etc/jupyter/nbgrader_config.py
     cat /course/etc.jupyter.nbgrader_config.py.append >> /etc/jupyter/nbgrader_config.py
-    echo >> "# end appended data"
+    echo "# end appended data" >> /etc/jupyter/nbgrader_config.py
 fi
