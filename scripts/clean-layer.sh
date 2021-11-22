216c2905642bef8afe113ba5cdb426f4cd80722a
Sami Laine <sami.v.laine@aalto.fi>
2021-11-22 15:56:17 +0200

    Remove outdated packages, buildable base image

--- scripts/clean-layer.sh
+++ scripts/clean-layer.sh
@@ -5,8 +5,8 @@ rm -rf /home/$NB_USER/.cache/yarn
 
 conda clean --all --yes
 mamba clean --all --yes
-mountpoint /opt/conda/pkgs/cache/ || rm -rf /opt/conda/pkgs/cache/
-mountpoint /root/.cache/pip/ || rm -rf /root/.cache/pip/*
+mountpoint -q /opt/conda/pkgs/cache/ || rm -rf /opt/conda/pkgs/cache/
+mountpoint -q /root/.cache/pip/ || rm -rf /root/.cache/pip/*
 npm cache clean --force
 
 apt-get autoremove -y
